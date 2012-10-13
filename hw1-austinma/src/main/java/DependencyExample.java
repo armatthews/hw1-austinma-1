4df1e18b33824a09a1459d80214a6a674eadbe05
armatthews <armatthe@ubuntu>
2012-10-12 23:57:53 -0400

    -

--- hw1-austinma/src/main/java/DependencyExample.java
+++ hw1-austinma/src/main/java/DependencyExample.java
@@ -1,26 +1,61 @@
 import java.io.StringReader;
+import java.util.*;
+import java.net.*;
+import java.io.*;
 
 import edu.stanford.nlp.ling.Word;
 import edu.stanford.nlp.objectbank.TokenizerFactory;
 import edu.stanford.nlp.process.PTBTokenizer.PTBTokenizerFactory;
 import edu.stanford.nlp.process.Tokenizer;
 
-/**
- * An example for Homework 0 of 11791 F12
- * 
- * @author Zi Yang <ziy@cs.cmu.edu>
- */
 public class DependencyExample {
 
-  /**
-   * Tokenize a sentence in the argument, and print out the tokens to the console.
-   * 
-   * @param args
-   *          Set the first argument as the sentence to be tokenized.
-   */
-  public static void main(String[] args) {
-    TokenizerFactory<Word> factory = PTBTokenizerFactory.newTokenizerFactory();
-    Tokenizer<Word> tokenizer = factory.getTokenizer(new StringReader(args[0]));
-    System.out.println(tokenizer.tokenize());
-  }
+	public static String requestURL(String URLString) throws Exception {
+		URL url = new URL(URLString);
+		URLConnection urlConn = url.openConnection();
+		BufferedReader in = new BufferedReader(new InputStreamReader(urlConn.getInputStream()));
+		StringBuilder r = new StringBuilder();
+
+		String inputLine;
+		while ((inputLine = in.readLine()) != null) {
+			r.append(inputLine);
+			r.append("\n");
+		}
+		in.close();
+
+		return r.toString();
+	}
+
+	public static boolean wikipediaTest(String possibleGene) {
+		// System.out.println(requestURL("http://en.wikipedia.org/w/index.php?search=alkaline+phosphatases"));
+		// regex for:
+		// <li><div class='mw-search-result-heading'><a
+		// href="/wiki/Alkaline_phosphatase"
+		// regex that for 'gene'
+		return false;
+	}
+
+	public static void main(String[] args) throws Exception {
+		TokenizerFactory<Word> factory = PTBTokenizerFactory.newTokenizerFactory();
+		PosTagNamedEntityRecognizer recognizer = new PosTagNamedEntityRecognizer();
+
+		TestSuite testSuite = new TestSuite("src/main/resources/sample.in.txt", "src/main/resources/sample.out.txt");
+		for (TestCase testCase : testSuite.getTestCases()) {
+			String sentence = testCase.input.getSentence();
+			String sentenceID = testCase.input.getID();
+
+			Tokenizer<Word> tokenizer = factory.getTokenizer(new StringReader(sentence));
+
+			Map<Integer, Integer> geneSpans = recognizer.getGeneSpans(sentence);
+			float fScore = testCase.getFScore(geneSpans);
+			if (fScore != 0.0f)
+				System.out.println(fScore);
+			for (Map.Entry<Integer, Integer> span : geneSpans.entrySet()) {
+				// int spanStart = span.getKey();
+				// int spanEnd = span.getValue();
+				// System.out.println(String.format("%s|%d %d|%s", sentenceID,
+				// spanStart, spanEnd, sentence.substring(spanStart, spanEnd)));
+			}
+		}
+	}
 }
